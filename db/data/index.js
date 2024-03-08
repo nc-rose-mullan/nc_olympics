3fbfcc5ead4e6c885aa3c1d4efb7581dd449c522
Rose Mullan <rfmullan@gmail.com>
2024-03-08 15:55:27 +0000

    Revert "add PG intro lecture content"
    This reverts commit 0aa790afe0551e232a4ae896a0803451c034174e.

--- db/data/index.js
+++
@@ -1,5 +0,0 @@
-const athletesData = require('./athletes-data.json');
-const eventsData = require('./events-data.json');
-const medalsData = require('./medals-data.json');
-
-module.exports = {athletesData, eventsData, medalsData};
--- example.sql
+++ example.sql
@@ -13,8 +13,8 @@ VALUES
 ('Tiddlywinks'),
 ('Hammerthrow'),
 ('Skipping'),
-('Programming: singles'),
-('Programming: doubles'),
+('Programming: solo'),
+('Programming: paired'),
 ('Standing long jump');
 
 CREATE TABLE athletes(
@@ -31,6 +31,9 @@ INSERT INTO athletes
    ('Rose', 'cool girl', 1, 3),
    ('Eli', 'spicy eelz', 11, 5),
    ('Haz', 'loves gaming', 5, 4);
+
+SELECT athlete_name, athlete_bio AS bio, years_competing, athletes.event_id FROM athletes
+JOIN events ON events.event_id = athletes.event_id;
 
 CREATE TABLE medals (
     medal_id SERIAL PRIMARY KEY,
@@ -66,3 +69,10 @@ VALUES
 (2, 4),
 (2, 4), 
 (3, 2);
+
+SELECT AVG(years_competing) FROM athletes;
+
+SELECT (athlete_name), COUNT(medals.medal_id) AS no_of_medals_won FROM athletes_medals
+JOIN medals ON medals.medal_id = athletes_medals.medal_id
+JOIN athletes ON athletes.athlete_id = athletes_medals.athlete_id
+GROUP BY athlete_name;
